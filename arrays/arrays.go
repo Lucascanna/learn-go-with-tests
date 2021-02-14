c66ef03cce284e022fbec728ce0fbf50b5422a86
luca.scannapieco <luca.scannapieco@mia-platform.eu>
2021-02-14 18:04:23 +0100

    add more on arrays

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -7,3 +7,24 @@ func Sum(numbers []int) (sum int) {
 	}
 	return
 }
+
+// SumAll function
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+// SumTails function
+func SumTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return
+}
--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestArraySum(t *testing.T) {
 	t.Run("sum array of any size", func(t *testing.T) {
@@ -10,6 +13,43 @@ func TestArraySum(t *testing.T) {
 
 		if actual != expected {
 			t.Errorf("Expected %q but found %q", expected, actual)
+		}
+	})
+}
+func TestArraySumAll(t *testing.T) {
+	t.Run("sum 3 arrays of any size", func(t *testing.T) {
+		arr1 := []int{1, 2, 3, 4}
+		arr2 := []int{1, 2, 3, 4, 5}
+		arr3 := []int{1, 2, 3}
+		actual := SumAll(arr1, arr2, arr3)
+		expected := []int{10, 15, 6}
+
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("Expected %v but found %v", expected, actual)
+		}
+	})
+}
+func TestArraySumTails(t *testing.T) {
+	t.Run("sum 3 tails of any size", func(t *testing.T) {
+		arr1 := []int{1, 2, 3, 4}
+		arr2 := []int{1, 2, 3, 4, 5}
+		arr3 := []int{1, 2, 3}
+		actual := SumTails(arr1, arr2, arr3)
+		expected := []int{9, 14, 5}
+
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("Expected %v but found %v", expected, actual)
+		}
+	})
+	t.Run("sum 3 tails of any size, one is empty", func(t *testing.T) {
+		arr1 := []int{1, 2, 3, 4}
+		arr2 := []int{}
+		arr3 := []int{1, 2, 3}
+		actual := SumTails(arr1, arr2, arr3)
+		expected := []int{9, 0, 5}
+
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("Expected %v but found %v", expected, actual)
 		}
 	})
 }
